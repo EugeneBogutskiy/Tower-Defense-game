cd1fcbd623c1929b4a04710105ae816e49dc603e
Eugene <EugeneBogutskiy@users.noreply.github.com>
2020-04-24 02:24:51 +0300

    Скрипт поведения врагов. Скорость, урон и т.д.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,21 +1,23 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public class EnemyMovement : MonoBehaviour
 {
-    public float speed = 10f;
     private Transform target;
     private int wayPointsIndex = 0;
 
+    private Enemy enemy;
+
     void Start()
     {
+        enemy = GetComponent<Enemy>();
         target = WayPoints.points[0];
     }
 
     void Update()
     {
         Vector3 direction = target.position - transform.position;
-        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        transform.Translate(direction.normalized * enemy.speed * Time.deltaTime, Space.World);
 
         if (Vector3.Distance(transform.position, target.position) <= 0.4f)
         {
@@ -28,6 +30,9 @@ public class EnemyMovement : MonoBehaviour
         if (wayPointsIndex >= WayPoints.points.Length - 1)
         {
             Destroy(gameObject);
+            GameObject burstGameObject = Instantiate(enemy.burstEffect, transform.position, enemy.burstEffect.transform.rotation);
+            Destroy(burstGameObject, 2f);
+            PlayerStats.lives--;
             return;
         }
 
