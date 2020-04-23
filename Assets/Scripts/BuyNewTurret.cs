e7e1548b1b3d68a888a9ed22c49ee9cd29e1bf41
Eugene <EugeneBogutskiy@users.noreply.github.com>
2020-04-24 02:23:35 +0300

    Скрипт улучшения туррели
    При наличии  необходимой суммы можно улучшить туррель просто на нее кликнув. При наведении на туррель мышки отображается стоимость улучшения

---
+++ Assets/Scripts/BuyNewTurret.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BuyNewTurret : MonoBehaviour
+{
+    public GameObject newTorretPrefab;
+    public GameObject buildTurretEffect;
+    public int costNewTurret = 500;
+
+    public GameObject costCanvas;
+    public Text costText;
+
+    void Start()
+    {
+        costText.text = costNewTurret.ToString();
+    }
+
+    void OnMouseEnter()
+    {
+        costCanvas.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        costCanvas.SetActive(false);
+    }
+
+    void OnMouseDown()
+    {
+        if (PlayerStats.gold < costNewTurret)
+        {
+            return;
+        }
+
+        PlayerStats.gold -= costNewTurret;
+        Instantiate(newTorretPrefab, transform.position, transform.rotation);
+        GameObject buildEffectGO = Instantiate(buildTurretEffect, transform.position, Quaternion.identity);
+        Destroy(buildEffectGO, 5f);
+        Destroy(gameObject);
+    }
+}
