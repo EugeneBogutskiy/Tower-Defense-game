9dd01b6ee78c6a08140c0b4a7e6866dfa25b578e
Eugene <EugeneBogutskiy@users.noreply.github.com>
2020-04-24 02:26:07 +0300

    Статистика игрока
    Здесь собраны и обрабатываются данные по игровому процессу

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    public static int gold;
+    public int startGold = 0;
+    public Text momeyText;
+
+    public static int lives;
+    public int startLives = 10;
+    public Text livesText;
+
+    public static int enemyСounter;
+
+    void Start()
+    {
+        gold = startGold;
+        lives = startLives;
+        enemyСounter = 0;
+    }
+
+    void Update()
+    {
+        momeyText.text = "Gold: " + gold.ToString();
+        livesText.text = "Lives: " + lives.ToString();
+    }
+}
