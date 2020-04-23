98f951b7f268c5ae83ceba7e4cb1ffe14d3d99c5
Eugene <EugeneBogutskiy@users.noreply.github.com>
2020-04-24 02:25:31 +0300

    Скрипт управляющий поведением игры
    Рестарт, окна и т.д.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameOverUI;
+    public Text scoreText;
+
+    private bool isGameEnded;
+
+    void Start()
+    {
+        isGameEnded = false;
+    }
+
+    void Update()
+    {
+        if (isGameEnded)
+        {
+            return;
+        }
+
+        if (PlayerStats.lives <= 0)
+        {
+            gameOverUI.SetActive(true);
+            EndGame();
+        }
+    }
+
+    void EndGame()
+    {
+        scoreText.text = "enemies killed: " + PlayerStats.enemyСounter.ToString();
+        isGameEnded = true;
+        Time.timeScale = 0;
+    }
+
+    public void Retry()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
