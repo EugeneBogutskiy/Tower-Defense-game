08e297cf208a8b49be4db334b0dcf896fdfe787e
Eugene <EugeneBogutskiy@users.noreply.github.com>
2020-04-24 02:22:18 +0300

    Скрипт настройки характеристики снарядов

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 50f;
+    public float damage = 20f;
+
+    private Transform target;
+    public GameObject impactEffect;
+
+    public void Seek(Transform target)
+    {
+        this.target = target;
+    }
+
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 direction = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (direction.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+    }
+
+    void HitTarget()
+    {
+        GameObject effectGameObject = Instantiate(impactEffect, target.position, target.rotation);
+        Destroy(effectGameObject, 1.5f);
+        Damage(target);
+        Destroy(gameObject);
+    }
+
+    void Damage(Transform enemy)
+    {
+        Enemy e = enemy.GetComponent<Enemy>();
+
+        if (e != null)
+        {
+            e.TakeDamage(damage);
+        }
+    }
+}
